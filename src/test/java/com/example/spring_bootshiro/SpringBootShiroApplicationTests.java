ef83e341f225853b864b256d9f93e4dc726bdbef
XuJ <xujin@startimes.com.cn>
2023-02-24 09:47:47 +0800

    Initial commit

---
+++ src/test/java/com/example/spring_bootshiro/SpringBootShiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_bootshiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootShiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
